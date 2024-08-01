eaf5cfacbcae06490cdc4b3041d22ca82da18c23
Victor Dircio <73125400+vdircio@users.noreply.github.com>
2024-08-01 19:59:21 +0000

    payment and random

--- app.rb
+++ app.rb
@@ -26,3 +26,29 @@ get("/square_root/results") do
   @square_rooted = @user_number ** 0.5
   erb(:square_root_results)
 end
+
+get("/payment/new") do
+  erb(:payment)
+end
+
+get("/payment/results") do
+  @apr = params.fetch("apr").to_f
+  @num_years = params.fetch("num_years").to_i
+  @principal = params.fetch("principal").to_f
+  @r = @apr / 100 / 12
+  @pv = @principal
+  @n = @num_years * 12
+  @payment = ((@r*@pv)/(1- ((1+@r) ** (-1 * @n)))).to_fs(:currency)
+  erb(:payment_results)
+end
+
+get('/random/new') do
+  erb(:random)
+end
+
+get('/random/results') do
+  @minimum = params.fetch("minimum").to_f
+  @maximum = params.fetch("maximum").to_f
+  @random_result = rand(@minimum...@maximum)
+  erb(:random_results)
+end
